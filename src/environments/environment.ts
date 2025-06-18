a27a0be94d25ffdecd8422f3fcf4adc68624bd46
Darius Tiganas <adarius9.1999@gmail.com>
2025-06-18 12:01:05 +0200

    Se ha creado la constante de los entornos para configurar los textos importantes

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+
+    production: true,
+    companyName: 'Gifs',
+    companyName2: 'App',
+    companySlogan: 'Aplicacion para buscar gifs',
+
+    // api keys
+
+    // urls
+};
