7eca33e55fcee4b170e1c3cc73fc8281c9e22997
Darius Tiganas <adarius9.1999@gmail.com>
2025-06-18 10:45:47 +0200

    Se ha creado la interfaz menuOption para configurar las rutas de "Trending" y "Search"

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,9 +1,37 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+interface MenuOption {
+  label: string,
+  subLabel: string,
+  route: string,
+  icon: string
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [
+    RouterLink, RouterLinkActive
+  ],
   templateUrl: './gifs-side-menu-options.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class GifsSideMenuOptionsComponent { }
+export default class GifsSideMenuOptionsComponent { 
+
+menuOptions:MenuOption[] = [
+  {
+    icon: 'fa-solid fa-chart-line',
+    label: 'Trending',
+    subLabel: 'Gifs Populares',
+    route: '/dashboard/trending'
+  },
+  {
+    icon: 'fa-solid fa-magnifying-glass',
+    label: 'Buscador',
+    subLabel: 'Buscar gifs',
+    route: '/dashboard/search'
+  }
+]
+
+}
