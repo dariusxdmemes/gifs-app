8e4cf4ffd49295a277b1bab14e1b4da610ee8be0
Darius Tiganas <adarius9.1999@gmail.com>
2025-06-18 12:00:00 +0200

    Se han declarado los entornos dentro de la clase

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
@@ -6,4 +7,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './gifs-side-menu-header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class GifsSideMenuHeaderComponent { }
+export default class GifsSideMenuHeaderComponent {
+  
+envs = environment;
+
+}
